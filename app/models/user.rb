61a3afe81b205fa77c829e9bd1b169d43c52b28c
Valerie Chatelet <vlrchtlt@gmail.com>
2015-09-01 12:16:53 +0200

    add new attributes to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable#, :validatable
+         :recoverable, :rememberable, :trackable,
+         :omniauthable, omniauth_providers: [:facebook] #, :validatable
 end
