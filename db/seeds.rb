d0fa040202476c29b5f80339f90d744a97e0fa17
Nathalie Mesnard <nathalie.mesnard@gmail.com>
2015-09-01 18:10:39 +0200

    finished seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Experience.destroy_all
+User.destroy_all
 Category.destroy_all
 
 # Seed for categories
@@ -17,22 +19,23 @@ names.each do |name|
 end
 
 
-users = []
-{ email: "nathy@gmail.com",
-  password: "blabla2045"
-},
-{ email: "anastasia@hotmail.com",
-  password: "blabrty209745"
-},
-{ email: "stefiam@gmail.com",
-  password: "blabrtfjk745":
-},
-{ email: "fjsqfkn@hotmail.com",
-  password: "blaEUII4dj"
-},
-{ email: "claireatoutbon@hotmail.com",
-  password: "bfmlffkly209745"
-}
+users = [
+  { email: "nathy@gmail.com",
+    password: "blabla2045"
+  },
+  { email: "anastasia@hotmail.com",
+    password: "blabrty209745"
+  },
+  { email: "stefiam@gmail.com",
+    password: "blabrtfjk745"
+  },
+  { email: "fjsqfkn@hotmail.com",
+    password: "blaEUII4dj"
+  },
+  { email: "claireatoutbon@hotmail.com",
+    password: "bfmlffkly209745"
+  }
+]
 
 users.each do |user|
   User.create(user)
@@ -44,31 +47,31 @@ experiences = [
     startdate: Time.zone.parse('2015-05-15'),
     enddate: Time.zone.parse('2015-10-31'),
     address: "Milan",
-    user_id: 1
+    user_id: User.last.id
   },
   { title: "Vibrer devant Agua de PINA BAUSCH",
     description: "Fièvre brésilienne : L’insouciance, la couleur, les rythmes pimentent le flot ininterrompu de cette pièce de Pina Bausch sous influence voyageuse.",
     startdate: Time.zone.parse('2016-05-07'),
     enddate: Time.zone.parse('2015-05-15'),
     address: "Théâtre de la Ville 75001 Paris",
-    user_id: 2
+    user_id: User.last.id - 1
   },
   { title: "Se dépasser au Marathon de Paris",
     description: "l’un des marathons les plus importants marathons du monde, tant par l’importance du peloton que par les performances réalisées.",
     startdate: Time.zone.parse('2016-04-03 09:00:00'),
     enddate: Time.zone.parse('2016-04-03 19.00.00'),
     address: "Paris",
-    user_id: 3
+    user_id: User.last.id - 2
   },
   { title: "danser dans les guinguettes des bords de Marne",
     description: "le patrimoine culturel et naturel des bords de Marne à travers les célèbres guinguettes, une nature abondante et une architecture éclectique.",
     address: "Saint-Maur-des-Fossés",
-    user_id: 4
+    user_id: User.last.id - 3
   },
   { title: "Vivre la fin du monde devant Mad Max: Fury Road",
     description: "L'univers est fabuleux, crasseux, poussiéreux, punk, malsain, glauque, les corps sont abimés, dégénérés, malades, la terre est acide...",
     address: "Cinéma des Cinéastes 7, av. de Clichy 75017 Paris",
-    user_id: 5
+    user_id: User.last.id - 4
   },
 ]
 
@@ -78,10 +81,12 @@ experiences.each do |experience|
 end
 
 # Add categories to experiences
-Experience.find(1).categories << Category.new(name: "exposition")
-Experience.find(1).categories << Category.new(name: "voyage")
-Experience.find(2).categories << Category.new(name: "sport")
-Experience.find(3).categories << Category.new(name: "spectacle")
+Experience.last.categories << Category.find_by(name: "cinéma")
+Experience.last(2).first.categories << Category.find_by(name: "balade")
+Experience.last(3).first.categories << Category.find_by(name: "sport")
+Experience.last(4).first.categories << Category.find_by(name: "spectacle")
+Experience.first.categories << Category.find_by(name: "voyage")
+Experience.first.categories << Category.find_by(name: "exposition")
 
 # Add user_id to experience
 
