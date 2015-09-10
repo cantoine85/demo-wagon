d0ef115b04176d0be2f96551ed05f7b52bb1edae
Valerie Chatelet <vlrchtlt@gmail.com>
2015-09-10 17:12:02 +0200

    bug à corriger dns banner

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -58,6 +58,7 @@ class ExperiencesController < ApplicationController
 
       @other_experience = Experience.find(ids[rand(0...ids.size)])
     else
+      @other_experience = nil
       @message = "nous n'avons plus d'expériences à vous proposer. Revenez très vite pour de nouvelles aventures"
     end
 
